9b3e6bd4e766716882439e88add8f50bd3d739d7
rwarou <rwarou.yim@gmail.com>
2021-12-21 16:14:38 +0900

    위젯화 시키고 회원가입 페이지로 카피

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -27,6 +27,10 @@ class _HomeState extends State<Home> {
     super.dispose();
   }
 
+  bool isZero() {
+    return _idController.text.isEmpty && _pwController.text.isEmpty;
+  }
+
   bool isLogin() {
     return _idController.text.length >= 4 && _pwController.text.length >= 4;
   }
@@ -37,7 +41,7 @@ class _HomeState extends State<Home> {
     double width = deviceSize.width;
     double height = deviceSize.height;
 
-    OutlineInputBorder border(TextEditingController controller) {
+    border(TextEditingController controller) {
       return OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
         borderSide: BorderSide(
@@ -49,11 +53,93 @@ class _HomeState extends State<Home> {
       );
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('LOGIN'),
-      ),
-      body: Center(
+    inputId() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: TextField(
+          controller: _idController,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'[a-z|A-Z|0-9]'))
+          ],
+          decoration: InputDecoration(
+            labelText: 'ID',
+            labelStyle: TextStyle(color: Colors.black.withOpacity(0.5)),
+            hintText: 'ENTER YOUR ID',
+            border: border(_idController),
+            focusedBorder: border(_idController),
+            enabledBorder: border(_idController),
+          ),
+          onChanged: (text) {
+            setState(() {});
+          },
+        ),
+      );
+    }
+
+    inputPw() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: TextField(
+          controller: _pwController,
+          obscureText: true,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(
+                RegExp(r'[\w|$`~!@$!%*#^?&\\(\\)\-_=+\[\]\{\}\;\:\\,\.\<\>]'))
+          ],
+          decoration: InputDecoration(
+            labelText: 'PASSWORD',
+            labelStyle: TextStyle(color: Colors.black.withOpacity(0.5)),
+            hintText: 'ENTER YOUR PASSWORD',
+            border: border(_pwController),
+            focusedBorder: border(_pwController),
+            enabledBorder: border(_pwController),
+          ),
+          onChanged: (text) {
+            setState(() {});
+          },
+        ),
+      );
+    }
+
+    loginButton() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: ElevatedButton(
+          onPressed: !isLogin()
+              ? null
+              : () {
+                  Get.toNamed('/dashboard');
+                  // Get.offNamed('/dashboard');
+                },
+          child: const Text('LOGIN'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.lightBlue,
+            onPrimary: Colors.white,
+            minimumSize: Size(width < 300 ? 250 : 300, 50),
+          ),
+        ),
+      );
+    }
+
+    registerButton() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: ElevatedButton(
+          onPressed: () {
+            Get.toNamed('/users/register');
+          },
+          child: const Text('REGISTER'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.lightBlue,
+            onPrimary: Colors.white,
+            minimumSize: Size(width < 300 ? 250 : 300, 50),
+          ),
+        ),
+      );
+    }
+
+    body() {
+      return Center(
         child: Container(
           width: width < 300 ? 250 : 300,
           child: Padding(
@@ -62,87 +148,26 @@ class _HomeState extends State<Home> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: TextField(
-                    controller: _idController,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.allow(
-                          RegExp(r'[a-z|A-Z|0-9]'))
-                    ],
-                    decoration: InputDecoration(
-                      labelText: 'ID',
-                      labelStyle:
-                          TextStyle(color: Colors.black.withOpacity(0.5)),
-                      hintText: 'ENTER YOUR ID',
-                      border: border(_idController),
-                      focusedBorder: border(_idController),
-                      enabledBorder: border(_idController),
-                    ),
-                    onChanged: (text) {
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: TextField(
-                    controller: _pwController,
-                    obscureText: true,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.allow(RegExp(
-                          r'[\w|$`~!@$!%*#^?&\\(\\)\-_=+\[\]\{\}\;\:\\,\.\<\>]'))
-                    ],
-                    decoration: InputDecoration(
-                      labelText: 'PASSWORD',
-                      labelStyle:
-                          TextStyle(color: Colors.black.withOpacity(0.5)),
-                      hintText: 'ENTER YOUR PASSWORD',
-                      border: border(_pwController),
-                      focusedBorder: border(_pwController),
-                      enabledBorder: border(_pwController),
-                    ),
-                    onChanged: (text) {
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: ElevatedButton(
-                    onPressed: !isLogin()
-                        ? null
-                        : () {
-                            Get.toNamed('/dashboard');
-                            // Get.offNamed('/dashboard');
-                          },
-                    child: const Text('LOGIN'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.lightBlue,
-                      onPrimary: Colors.white,
-                      minimumSize: Size(width < 300 ? 250 : 300, 50),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Get.toNamed('/users/register');
-                    },
-                    child: const Text('REGISTER'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.lightBlue,
-                      onPrimary: Colors.white,
-                      minimumSize: Size(width < 300 ? 250 : 300, 50),
-                    ),
-                  ),
-                ),
+                inputId(),
+                inputPw(),
+                loginButton(),
+                registerButton(),
               ],
             ),
           ),
         ),
-      ),
+      );
+    }
+
+    appBar() {
+      return AppBar(
+        title: const Text('LOGIN'),
+      );
+    }
+
+    return Scaffold(
+      appBar: appBar(),
+      body: body(),
     );
   }
 }
--- lib/pages/users/register.dart
+++ lib/pages/users/register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -8,13 +10,187 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final _idController = TextEditingController();
+  final _pwController = TextEditingController();
+  final _nameController = TextEditingController();
+  String id = '';
+  String pw = '';
+  String name = '';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _idController.dispose();
+    _pwController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  bool isZero() {
+    return _idController.text.isEmpty && _pwController.text.isEmpty;
+  }
+
+  bool isRegister() {
+    return _idController.text.length >= 4 &&
+        _pwController.text.length >= 4 &&
+        _nameController.text.isNotEmpty;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    Size deviceSize = MediaQuery.of(context).size;
+    double width = deviceSize.width;
+    double height = deviceSize.height;
+
+    border(TextEditingController controller) {
+      return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(
+          width: 1,
+          color: controller.text.length >= 4
+              ? Colors.lightBlue.withOpacity(0.5)
+              : Colors.red.withOpacity(0.5),
+        ),
+      );
+    }
+
+    nameBorder(TextEditingController controller) {
+      return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(
+          width: 1,
+          color: controller.text.isNotEmpty
+              ? Colors.lightBlue.withOpacity(0.5)
+              : Colors.red.withOpacity(0.5),
+        ),
+      );
+    }
+
+    inputId() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: TextField(
+          controller: _idController,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'[a-z|A-Z|0-9]'))
+          ],
+          decoration: InputDecoration(
+            labelText: 'ID',
+            labelStyle: TextStyle(color: Colors.black.withOpacity(0.5)),
+            hintText: 'ENTER YOUR ID',
+            border: border(_idController),
+            focusedBorder: border(_idController),
+            enabledBorder: border(_idController),
+          ),
+          onChanged: (text) {
+            setState(() {});
+          },
+        ),
+      );
+    }
+
+    inputPw() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: TextField(
+          controller: _pwController,
+          obscureText: true,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(
+                RegExp(r'[\w|$`~!@$!%*#^?&\\(\\)\-_=+\[\]\{\}\;\:\\,\.\<\>]'))
+          ],
+          decoration: InputDecoration(
+            labelText: 'PASSWORD',
+            labelStyle: TextStyle(color: Colors.black.withOpacity(0.5)),
+            hintText: 'ENTER YOUR PASSWORD',
+            border: border(_pwController),
+            focusedBorder: border(_pwController),
+            enabledBorder: border(_pwController),
+          ),
+          onChanged: (text) {
+            setState(() {});
+          },
+        ),
+      );
+    }
+
+    inputName() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: TextField(
+          controller: _nameController,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|a-z|A-Z]'))
+          ],
+          decoration: InputDecoration(
+            labelText: 'NAME',
+            labelStyle: TextStyle(color: Colors.black.withOpacity(0.5)),
+            hintText: 'ENTER YOUR NAME',
+            border: nameBorder(_nameController),
+            focusedBorder: nameBorder(_nameController),
+            enabledBorder: nameBorder(_nameController),
+          ),
+          onChanged: (text) {
+            setState(() {});
+          },
+        ),
+      );
+    }
+
+    registerButton() {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: ElevatedButton(
+          onPressed: !isRegister()
+              ? null
+              : () {
+                  Get.toNamed('/dashboard');
+                  // Get.offNamed('/dashboard');
+                },
+          child: const Text('REGISTER'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.lightBlue,
+            onPrimary: Colors.white,
+            minimumSize: Size(width < 300 ? 250 : 300, 50),
+          ),
+        ),
+      );
+    }
+
+    body() {
+      return Center(
+        child: Container(
+          width: width < 300 ? 250 : 300,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                inputId(),
+                inputPw(),
+                inputName(),
+                registerButton(),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    appBar() {
+      return AppBar(
         title: Text('REGISTER'),
-      ),
-      body: Text('register page'),
+      );
+    }
+
+    return Scaffold(
+      appBar: appBar(),
+      body: body(),
     );
   }
 }
